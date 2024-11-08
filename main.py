6524540db1ea91cd86a0e39ed70028acdcd2327c
kareem <kareem.abdelnabi.22@gmail.com>
2024-11-08 22:46:50 +0200

    finish spectrograms show and hide

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFrame, QPushButton
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QIcon
 
 from icons_setup.compiledIcons import *
+from helper_function.compile_qrc import compile_qrc
+compile_qrc()
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -12,6 +14,23 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Equalizer')
         self.setWindowIcon(QIcon('icons_setup\icons\logo.png'))
 
+        self.isSpectrogramDisplayed = True
+
+        self.showIcon = QIcon('icons_setup\icons\show.png')
+        self.hideIcon = QIcon('icons_setup\icons\hide.png')
+
+        self.spectrogramsFrame = self.findChild(QFrame, 'spectrogramsFrame')
+        self.spectrogramDisplayButton = self.findChild(QPushButton, 'spectrogramDisplayButton')
+        self.spectrogramDisplayButton.clicked.connect(self.toggleSpectrogramDisplay)
+        
+    def toggleSpectrogramDisplay(self):
+        if self.isSpectrogramDisplayed:
+            self.spectrogramsFrame.hide()
+            self.spectrogramDisplayButton.setIcon(self.showIcon)
+        else:
+            self.spectrogramsFrame.show()
+            self.spectrogramDisplayButton.setIcon(self.hideIcon)
+        self.isSpectrogramDisplayed = not self.isSpectrogramDisplayed
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
