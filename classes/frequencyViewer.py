65c61b83608bc3a9e8d1cc2a52a2b42aadb7a5d2
Mostafaali3 <mostafaalisarhan@gmail.com>
2024-11-08 21:53:37 +0200

    frequency viewer

--- classes/frequencyViewer.py
+++ classes/frequencyViewer.py
@@ -1,9 +1,9 @@
-from CustomSignal import CustomSignal
+from classes.customSignal import CustomSignal
 import pyqtgraph as pg
 import numpy as np
 
 class FrequencyViewer(pg.PlotWidget):
-    def __init__(self , current_signal:CustomSignal , scale = 'linear'):
+    def __init__(self , current_signal:CustomSignal = None, scale = 'linear'):
         super().__init__()
         self.current_signal = current_signal
         self.showGrid(x= True, y= True , alpha = 0.25)
@@ -20,3 +20,13 @@ class FrequencyViewer(pg.PlotWidget):
             current_signal_audiogram_x_values = 20 * np.log10(self.current_signal.new_linear_frequency[0])
             self.plot(current_signal_audiogram_x_values , signal_rfft_result_magnitudes, pen=pg.mkPen(color = 'b' , width=1))
             # self.setYRange(min(self.current_signal.new_linear_frequency[1]),max(self.current_signal.new_linear_frequency[1]))
+            
+            
+    @property
+    def current_signal(self):
+        return self.__current_signal
+    
+    @current_signal.setter
+    def current_signal(self, new_signal):
+        self.__current_signal = new_signal
+    
