372f69f90fabc97c8c6f30da12045cab7eb39b91
kareem <kareem.abdelnabi.22@gmail.com>
2024-11-08 12:34:28 +0200

    UI setup

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import QIcon
+
+from icons_setup.compiledIcons import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi('main.ui', self)
+        self.setWindowTitle('Equalizer')
+        self.setWindowIcon(QIcon('icons_setup\icons\logo.png'))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
