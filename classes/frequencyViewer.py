be00a51e9bbce15b8e22a61fe641ab6b08453351
Mostafaali3 <mostafaalisarhan@gmail.com>
2024-11-13 14:59:04 +0200

    colors modifications

--- classes/frequencyViewer.py
+++ classes/frequencyViewer.py
@@ -18,7 +18,7 @@ class FrequencyViewer(pg.PlotWidget):
                 self.addItem(pg.InfiniteLine(pos=x_value, angle=90, pen=pg.mkPen(color='r', width=2)))
             self.setLogMode(x=False, y=False)
             self.invertY(False)
-            self.plot(self.current_signal.new_linear_frequency[0] , list(signal_rfft_result_magnitudes), pen=pg.mkPen(color = 'b' , width=1))
+            self.plot(self.current_signal.new_linear_frequency[0] , list(signal_rfft_result_magnitudes), pen=pg.mkPen(color = 'w' , width=3))
             # self.setYRange(min(self.current_signal.new_linear_frequency[1]),max(self.current_signal.new_linear_frequency[1]))
         elif(self.view_scale == "Audiogram"):
             signal_rfft_result_magnitudes_after_clipping = np.clip(signal_rfft_result_magnitudes, a_min=1e-10, a_max=None)
@@ -28,7 +28,7 @@ class FrequencyViewer(pg.PlotWidget):
             # self.setRange(xRange = [min(self.frequency_boundaries) , max(self.frequency_boundaries)] , yRange =[min(signal_rfft_result_magnitudes_db_scale) , max(signal_rfft_result_magnitudes_db_scale)] )
             self.setLogMode(x=True, y=False)
             self.invertY(True)
-            self.plot(self.current_signal.new_linear_frequency[0] , signal_rfft_result_magnitudes_db_scale, pen=pg.mkPen(color = 'b' , width=1))
+            self.plot(self.current_signal.new_linear_frequency[0] , signal_rfft_result_magnitudes_db_scale, pen=pg.mkPen(color = 'w' , width=3))
             # self.setYRange(min(self.current_signal.new_linear_frequency[1]),max(self.current_signal.new_linear_frequency[1]))
             
     @property
